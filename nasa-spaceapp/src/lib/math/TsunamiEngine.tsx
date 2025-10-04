0111aeafdaf500b8a3a907b15a17ceca59ec94c4
AlanPosselt <alanposselt06@gmail.com>
2025-10-04 15:04:18 -0300

    Create TsunamiEngine.tsx

---
+++ nasa-spaceapp/src/lib/math/TsunamiEngine.tsx
@@ -0,0 +1,5 @@
+// src/lib/tsunami/TsunamiEngine.tsx
+// Se você NÃO usa alias '@/' no tsconfig, troque a linha de cima por:
+// import type { TsunamiInput, TsunamiPointQuery } from "./types";
+
+export { TsunamiEngine } from "./TsunamiEngine";
